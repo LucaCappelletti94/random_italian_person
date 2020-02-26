b86035cd34a63ee3df4d4a518610dcd62ce9e7f1
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-02-26 11:33:45 +0100

    Reduced code duplication

--- random_italian_person/utils/random_birthday.py
+++ random_italian_person/utils/random_birthday.py
@@ -7,8 +7,10 @@ def random_birthday():
     """
     This function will return a random datetime between two datetime objects.
     """
-    start = datetime.now() - timedelta(seconds=24*60*60*365*100)
-    end = datetime.now() - timedelta(seconds=24*60*60*365*18)
+    now = datetime.now()
+    seconds_in_year = 24*60*60*365
+    start = now - timedelta(seconds=seconds_in_year*100)
+    end = now - timedelta(seconds=seconds_in_year*18)
     delta = end - start
     int_delta = (delta.days * 24 * 60 * 60) + delta.seconds
     random_second = randrange(int_delta)
