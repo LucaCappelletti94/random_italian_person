1fcf85721508f15c310c3a2a3567e53f8857b56d
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-02-26 11:24:34 +0100

    Added support for plausible address

--- random_italian_person/random_italian_person.py
+++ random_italian_person/random_italian_person.py
@@ -12,8 +12,10 @@ class RandomItalianPerson:
     municipalities = None
     names = None
     surnames = None
+    addresses = None
 
     def __init__(self):
+        """Create a new random Italian person."""
         if RandomItalianPerson.municipalities is None:
             RandomItalianPerson.municipalities = pd.read_csv(
                 "{}/datasets/municipalities.csv".format(
@@ -37,6 +39,13 @@ class RandomItalianPerson:
                     os.path.dirname(os.path.abspath(__file__)))
             )
 
+        if RandomItalianPerson.addresses is None:
+            RandomItalianPerson.addresses = pd.read_csv(
+                "{}/datasets/addresses.csv".format(
+                    os.path.dirname(os.path.abspath(__file__))),
+                dtype={"cap": str}
+            )
+
         surname_data = RandomItalianPerson.surnames.sample(n=1)
         name_data = RandomItalianPerson.names.iloc[np.random.choice(
             RandomItalianPerson.names.index, p=RandomItalianPerson.frequencies)]
@@ -45,11 +54,17 @@ class RandomItalianPerson:
                 surname_data.province)
         ].sample(n=1)
 
+        address_data = RandomItalianPerson.addresses.sample(n=1)
+
         self._data = {
             **surname_data.reset_index(drop=True).iloc[0].to_dict(),
             **name_data.to_dict(),
-            **municipality_data.reset_index(drop=True).iloc[0].to_dict(),
-            "birthdate": random_birthday().isoformat()
+            **{
+                "birth_{}".format(c): v
+                for c, v in municipality_data.reset_index(drop=True).iloc[0].to_dict().items()
+            },
+            "birthdate": random_birthday().isoformat(),
+            **address_data.reset_index(drop=True).iloc[0].to_dict()
         }
 
         self._data["codice_fiscale"] = codicefiscale.encode(
@@ -57,7 +72,7 @@ class RandomItalianPerson:
             name=self.name,
             sex=self.sex,
             birthdate=self.birthdate,
-            birthplace=self.municipality
+            birthplace=self.birthplace
         )
 
     @property
@@ -69,20 +84,8 @@ class RandomItalianPerson:
         return self._data["surname"]
 
     @property
-    def province(self) -> str:
-        return self._data["province"]
-
-    @property
-    def province_code(self) -> str:
-        return self._data["province"]
-
-    @property
-    def region(self) -> str:
-        return self._data["region"]
-
-    @property
-    def municipality(self) -> str:
-        return self._data["municipality"]
+    def birthplace(self) -> str:
+        return self._data["birth_municipality"]
 
     @property
     def birthdate(self) -> str:
@@ -93,12 +96,15 @@ class RandomItalianPerson:
         return self._data["sex"]
 
     @property
-    def codice_fiscale(self) -> str:
-        return self._data["codice_fiscale"]
-
-    @property
     def data(self) -> Dict:
-        return self._data
+        return self._data.copy()
+
+    def describe(self) -> str:
+        """Return short paragraph with human readable description of the person."""
+        return (
+            "{name} {surname} è nata/o a {birth_municipality} ({birth_province_code}) il {birthdate}."
+            " Ora vive a {municipality} ({province_code}) in {address} {house_number}."
+        ).format(**self._data)
 
     def __repr__(self) -> str:
         return json.dumps(self._data, indent=4)
